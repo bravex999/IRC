4f476486255624763b0695437c403454b6d013ca
naranjox <182925606+bravex999@users.noreply.github.com>
2026-02-16 17:26:30 +0100

    class Server and Room

---
+++ ChatRoom.hpp
@@ -0,0 +1,50 @@
+#ifndef CHATROOM_HPP
+#define CHATROOM_HPP
+
+#include "UserConn.hpp"
+#include <vector>
+#include <string>
+
+class ChatRoom
+{
+private:
+	std::string				_name;
+	std::string				_topic;
+	std::string				_key;
+	int						_limit;
+	bool					_inviteMode;
+	bool					_topicMode;
+	std::vector<UserConn*>	_members;
+	std::vector<UserConn*>	_admins;
+
+public:
+	ChatRoom(const std::string &name);
+	~ChatRoom();
+
+	std::string				getName() const;
+	std::string				getTopic() const;
+	std::string				getKey() const;
+	int						getLimit() const;
+	bool					isInviteOnly() const;
+	bool					isTopicRestricted() const;
+	std::string				getModeString() const;
+	std::string				getUserListStr() const;
+	std::vector<UserConn*>	getMembers();
+
+	void	setTopic(const std::string &t);
+	void	setKey(const std::string &k);
+	void	setLimit(int l);
+	void	setInviteMode(bool on);
+	void	setTopicMode(bool on);
+
+	void	addMember(UserConn *u);
+	void	removeMember(UserConn *u);
+	void	addAdmin(UserConn *u);
+	void	removeAdmin(UserConn *u);
+
+	bool	isMember(UserConn *u) const;
+	bool	isAdmin(UserConn *u) const;
+	bool	isFull() const;
+};
+
+#endif
---
+++ IrcServer.hpp
@@ -0,0 +1,60 @@
+#ifndef IRCSERVER_HPP
+#define IRCSERVER_HPP
+
+#include <vector>
+#include <string>
+#include <poll.h>
+#include "UserConn.hpp"
+#include "ChatRoom.hpp"
+
+class IrcServer
+{
+private:
+	int						_port;
+	std::string				_pass;
+	int						_serverSocket;
+	std::vector<pollfd>		_pollFds;
+	std::vector<UserConn*>	_clients;
+	std::vector<ChatRoom*>	_channels;
+	static bool				_running;
+
+	void	_initSocket();
+	void	_runLoop();
+	void	_acceptClient();
+	void	_handleData(int fd);
+	void	_removeClient(int fd, const std::string &reason = "Quit");
+
+	void	_exec(const std::string &line, int fd);
+	void	_send(int fd, const std::string &msg);
+	void	_broadcast(const std::string &msg, ChatRoom *chan, int excludeFd);
+
+	void	_flushOut(int fd);
+	void	_compactPollFds();
+	pollfd	*_getPollFd(int fd);
+
+	UserConn*	_getClient(int fd);
+	UserConn*	_getClient(const std::string &nick);
+	ChatRoom*	_getChannel(const std::string &name);
+	ChatRoom*	_createChannel(const std::string &name);
+
+	void	_cmdPass(UserConn *c, const std::string &args);
+	void	_cmdNick(UserConn *c, const std::string &args);
+	void	_cmdUser(UserConn *c, const std::string &args);
+	void	_cmdJoin(UserConn *c, const std::string &args);
+	void	_cmdPart(UserConn *c, const std::string &args);
+	void	_cmdKick(UserConn *c, const std::string &args);
+	void	_cmdInvite(UserConn *c, const std::string &args);
+	void	_cmdTopic(UserConn *c, const std::string &args);
+	void	_cmdMode(UserConn *c, const std::string &args);
+	void	_cmdPrivMsg(UserConn *c, const std::string &args);
+	void	_cmdQuit(UserConn *c, const std::string &args);
+
+	static void	_signalHandler(int signum);
+
+public:
+	IrcServer(int port, const std::string &pass);
+	~IrcServer();
+	void	start();
+};
+
+#endif
