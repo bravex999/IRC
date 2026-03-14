a0eccb30b7bf1cd951d93b1811179950138d932a
naranjox <182925606+bravex999@users.noreply.github.com>
2026-03-14 09:57:17 +0100

    fix : validation input atoi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,3 @@
-#include "../includes/IrcServer.hpp"
-#include <iostream>
-#include <cstdlib>
-
 int main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -9,7 +5,25 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
 	}
-	IrcServer server(std::atoi(argv[1]), argv[2]);
+
+	std::string portStr = argv[1];
+	for (size_t i = 0; i < portStr.length(); ++i)
+	{
+		if (portStr[i] < '0' || portStr[i] > '9')
+		{
+			std::cerr << "Error: invalid port" << std::endl;
+			return 1;
+		}
+	}
+
+	int port = std::atoi(argv[1]);
+	if (port < 1 || port > 65535)
+	{
+		std::cerr << "Error: port out of range (1-65535)" << std::endl;
+		return 1;
+	}
+
+	IrcServer server(port, argv[2]);
 	try
 	{
 		server.start();
@@ -20,4 +34,3 @@ int main(int argc, char **argv)
 	}
 	return 0;
 }
-
